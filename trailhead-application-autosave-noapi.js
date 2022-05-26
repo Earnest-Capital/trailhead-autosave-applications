d03e54f6eb0d97f0326ded494fa66919d8acec43
Michael Rouveure <michaelrouveure@hotmail.com>
2022-05-26 16:07:40 -0600

    status update

--- trailhead-application-autosave-noapi.js
+++ trailhead-application-autosave-noapi.js
@@ -64,32 +64,18 @@ function statusSubmitApplication(inputTagline, inputBusiness, inputProductLive,i
         
  if (!fieldTagline.value || !fieldBusiness.value || !fieldProductLive.value || !fieldMetrics.value || !fieldTeamBackground.value || !fieldUnfairAdvantage.value || !fieldPricingModel.value || !fieldCustomerSources.value || !fieldCompetition.value || !fieldRaisingCapital.value || !fieldRaising.value || !fieldRoundProcess.value || !fieldPreviousCapital.value) 
       {
-        //document.querySelector(statusTextIncomplete).innerHTML= "[Incomplete]";
-        document.querySelector("#container-div-submit-application").style.display = "none";
-        //document.querySelector("#div-mobile-submit-application").style.display = "none";
-        document.querySelector("#div-popup-submit-application").style.display = "none";
-        //document.querySelector("#div-popupmobile-submit-application").style.display = "none";
-        document.querySelector("#container-div-application-notready").style.display = "block";
-        //document.querySelector("#div-mobile-application-notready").style.display = "flex";
-        document.querySelector("#div-popup-application-notready").style.display = "flex";
-        //document.querySelector("#div-popupmobile-application-notready").style.display = "flex";
-        //console.log("true");
+       //tasks
+       document.querySelector("#status-submit-ready").style.display = "none";
+       document.querySelector("#status-submit-not-ready").style.display = "flex";
 
 
         
       }
  else
       {
-        //document.querySelector(statusTextComplete).innerHTML= "[Complete]";
-        document.querySelector("#container-div-application-notready").style.display = "none";
-        //document.querySelector("#div-mobile-application-notready").style.display = "none";
-        document.querySelector("#div-popup-application-notready").style.display = "none";
-        //document.querySelector("#div-popupmobile-application-notready").style.display = "none";
-        document.querySelector("#container-div-submit-application").style.display = "block";
-        //document.querySelector("#div-mobile-submit-application").style.display = "flex";
-        document.querySelector("#div-popup-submit-application").style.display = "flex";
-        //document.querySelector("#div-popupmobile-submit-application").style.display = "flex";
-        //console.log("false");
+        //tasks
+        document.querySelector("#status-submit-not-ready").style.display = "none";
+        document.querySelector("#status-submit-ready").style.display = "flex";
        
       }
  
@@ -107,43 +93,60 @@ function statusValueProposition(inputTagline, inputBusiness, inputProductLive)
         
  if (!fieldTagline.value || !fieldBusiness.value || !fieldProductLive.value) 
       {
-        //document.querySelector(statusTextIncomplete).innerHTML= "[Incomplete]";
+        
+        //titles
         document.querySelector("#title-status-productvalue-complete").style.display = "none";
         document.querySelector("#title-status-productvalue-incomplete").style.display = "flex";
+
+        //tasks
+        document.querySelector("#tasks-status-productvalue-complete").style.display = "none";
+        document.querySelector("#tasks-status-productvalue-incomplete").style.display = "flex";
  
       }
  else
       {
-        //document.querySelector(statusTextComplete).innerHTML= "[Complete]";
+        //titles
         document.querySelector("#title-status-productvalue-incomplete").style.display = "none";
         document.querySelector("#title-status-productvalue-complete").style.display = "flex";
+
+        //tasks
+        document.querySelector("#tasks-status-productvalue-incomplete").style.display = "none";
+        document.querySelector("#tasks-status-productvalue-complete").style.display = "flex";
       }
  
       }
 
       // create function: status update - traction & metrics
-function statusTractionMetrics(inputMetrics, statusTextIncomplete, statusTextComplete)
+function statusTractionMetrics(inputMetrics)
 {
   var fieldMetrics = document.querySelector(inputMetrics);
   
   
 if (!fieldMetrics.value) 
 {
-  document.querySelector(statusTextIncomplete).innerHTML= "[Incomplete]";
-  document.querySelector(statusTextComplete).style.display = "none";
-  document.querySelector(statusTextIncomplete).style.display = "block";
+  //titles
+  document.querySelector("#title-status-metrics-complete").style.display = "none";
+  document.querySelector("#title-status-metrics-incomplete").style.display = "flex";
+
+  //tasks
+  document.querySelector("#tasks-status-metrics-complete").style.display = "none";
+  document.querySelector("#tasks-status-metrics-incomplete").style.display = "flex";
 }
 else
 {
-  document.querySelector(statusTextComplete).innerHTML= "[Complete]";
-  document.querySelector(statusTextIncomplete).style.display = "none";
-  document.querySelector(statusTextComplete).style.display = "block";
+  //titles
+  document.querySelector("#title-status-metrics-incomplete").style.display = "none";
+  document.querySelector("#title-status-metrics-complete").style.display = "flex";
+
+  //tasks
+  document.querySelector("#tasks-status-metrics-incomplete").style.display = "none";
+  document.querySelector("#tasks-status-metrics-complete").style.display = "flex";
 }
 
 }
 
 // create function: status update - my team
-function statusTeam(inputTeamBackground, inputUnfairAdvantage, statusTextIncomplete, statusTextComplete)
+function statusTeam(inputTeamBackground, inputUnfairAdvantage)
       {
         var fieldTeamBackground = document.querySelector(inputTeamBackground);
         
@@ -152,22 +155,30 @@ function statusTeam(inputTeamBackground, inputUnfairAdvantage, statusTextIncompl
         
  if (!fieldTeamBackground.value || !fieldUnfairAdvantage.value) 
       {
-        document.querySelector(statusTextIncomplete).innerHTML= "[Incomplete]";
-        document.querySelector(statusTextComplete).style.display = "none";
-        document.querySelector(statusTextIncomplete).style.display = "block";
+        //titles
+        document.querySelector("#title-status-team-complete").style.display = "none";
+        document.querySelector("#title-status-team-incomplete").style.display = "flex";
+
+        //tasks
+        document.querySelector("#tasks-status-team-complete").style.display = "none";
+        document.querySelector("#tasks-status-team-incomplete").style.display = "flex";
  
       }
  else
       {
-        document.querySelector(statusTextComplete).innerHTML= "[Complete]";
-        document.querySelector(statusTextIncomplete).style.display = "none";
-        document.querySelector(statusTextComplete).style.display = "block";
+        //titles
+        document.querySelector("#title-status-team-incomplete").style.display = "none";
+        document.querySelector("#title-status-team-complete").style.display = "flex";
+
+  //tasks
+        document.querySelector("#tasks-status-team-incomplete").style.display = "none";
+        document.querySelector("#tasks-status-team-complete").style.display = "flex";
       }
  
       }
 
       // create function: status update - pricing & customers
-function statusPricing(inputPricingModel, inputCustomerSources, statusTextIncomplete, statusTextComplete)
+function statusPricing(inputPricingModel, inputCustomerSources)
 {
   var fieldPricingModel = document.querySelector(inputPricingModel);
   
@@ -176,43 +187,59 @@ function statusPricing(inputPricingModel, inputCustomerSources, statusTextIncomp
   
 if (!fieldPricingModel.value || !fieldCustomerSources.value) 
 {
-  document.querySelector(statusTextIncomplete).innerHTML= "[Incomplete]";
-  document.querySelector(statusTextComplete).style.display = "none";
-  document.querySelector(statusTextIncomplete).style.display = "block";
+  //titles
+  document.querySelector("#title-status-pricing-complete").style.display = "none";
+  document.querySelector("#title-status-pricing-incomplete").style.display = "flex";
+
+  //tasks
+  document.querySelector("#tasks-status-pricing-complete").style.display = "none";
+  document.querySelector("#tasks-status-pricing-incomplete").style.display = "flex";
 
 }
 else
 {
-  document.querySelector(statusTextComplete).innerHTML= "[Complete]";
-  document.querySelector(statusTextIncomplete).style.display = "none";
-  document.querySelector(statusTextComplete).style.display = "block";
+  //titles
+  document.querySelector("#title-status-pricing-incomplete").style.display = "none";
+  document.querySelector("#title-status-pricing-complete").style.display = "flex";
+
+  //tasks
+  document.querySelector("#tasks-status-pricing-incomplete").style.display = "none";
+  document.querySelector("#tasks-status-pricing-complete").style.display = "flex";
 }
 
 }
 
 // create function: status update - competition
-function statusCompetition(inputCompetition, statusTextIncomplete, statusTextComplete)
+function statusCompetition(inputCompetition)
 {
   var fieldCompetition = document.querySelector(inputCompetition);
   
   
 if (!fieldCompetition.value) 
 {
-  document.querySelector(statusTextIncomplete).innerHTML= "[Incomplete]";
-  document.querySelector(statusTextComplete).style.display = "none";
-  document.querySelector(statusTextIncomplete).style.display = "block";
+  //titles
+  document.querySelector("#title-status-competition-complete").style.display = "none";
+  document.querySelector("#title-status-competition-incomplete").style.display = "flex";
+
+  //tasks
+  document.querySelector("#tasks-status-competition-complete").style.display = "none";
+  document.querySelector("#tasks-status-competition-incomplete").style.display = "flex";
 }
 else
 {
-  document.querySelector(statusTextComplete).innerHTML= "[Complete]";
-  document.querySelector(statusTextIncomplete).style.display = "none";
-  document.querySelector(statusTextComplete).style.display = "block";
+  //titles
+  document.querySelector("#title-status-competition-incomplete").style.display = "none";
+  document.querySelector("#title-status-competition-complete").style.display = "flex";
+
+  //tasks
+  document.querySelector("#tasks-status-competition-incomplete").style.display = "none";
+  document.querySelector("#tasks-status-competition-complete").style.display = "flex";
 }
 
 }
 
 // create function: status update - fundraising
-function statusFundraising(inputRaisingCapital, inputRaising, inputRoundProcess, inputPreviousCapital, statusTextIncomplete, statusTextComplete)
+function statusFundraising(inputRaisingCapital, inputRaising, inputRoundProcess, inputPreviousCapital)
       {
         var fieldRaisingCapital = document.querySelector(inputRaisingCapital);
         
@@ -224,16 +251,24 @@ function statusFundraising(inputRaisingCapital, inputRaising, inputRoundProcess,
         
  if (!fieldRaisingCapital.value || !fieldRaising.value || !fieldRoundProcess.value || !fieldPreviousCapital.value) 
       {
-        document.querySelector(statusTextIncomplete).innerHTML= "[Incomplete]";
-        document.querySelector(statusTextComplete).style.display = "none";
-        document.querySelector(statusTextIncomplete).style.display = "block";
+        //titles
+        document.querySelector("#title-status-fundraising-complete").style.display = "none";
+        document.querySelector("#title-status-fundraising-incomplete").style.display = "flex";
+
+        //tasks
+        document.querySelector("#tasks-status-fundraising-complete").style.display = "none";
+        document.querySelector("#tasks-status-fundraising-incomplete").style.display = "flex";
  
       }
  else
       {
-        document.querySelector(statusTextComplete).innerHTML= "[Complete]";
-        document.querySelector(statusTextIncomplete).style.display = "none";
-        document.querySelector(statusTextComplete).style.display = "block";
+         //titles
+          document.querySelector("#title-status-fundraising-incomplete").style.display = "none";
+          document.querySelector("#title-status-fundraising-complete").style.display = "flex";
+
+         //tasks
+          document.querySelector("#tasks-status-fundraising-incomplete").style.display = "none";
+          document.querySelector("#tasks-status-fundraising-complete").style.display = "flex";
       }
  
       }
@@ -278,19 +313,19 @@ function setupAutosaveField(fieldSelector, validateFieldFn, airtableField, webfl
             statusValueProposition("#Tagline","#Business-Model","#Is-your-product-live-in-the-market");
             
             // status check: traction & metrics
-            statusTractionMetrics("#Key-Metrics","#status-tractionmetrics-incomplete","#status-tractionmetrics-complete");
+            statusTractionMetrics("#Key-Metrics");
 
             // status check: my team
-            statusTeam("#Founder-Team-Background","#Unfair-Advantage","#status-team-incomplete","#status-team-complete");
+            statusTeam("#Founder-Team-Background","#Unfair-Advantage");
 
             // status check: pricing & customers
-            statusPricing("#Pricing-Model","#customer-acquisition-sources","#status-pricing-incomplete","#status-pricing-complete");
+            statusPricing("#Pricing-Model","#customer-acquisition-sources");
 
             // status check: competition
-            statusCompetition("#Competition-Positioning","#status-competition-incomplete","#status-competition-complete");
+            statusCompetition("#Competition-Positioning");
 
             // status check: fundraising
-            statusFundraising("#Why-Rise-Capital","#Capital-Raising","#Round-Process","#Previous-Capital-Raised","#status-fundraising-incomplete","#status-fundraising-complete");
+            statusFundraising("#Why-Rise-Capital","#Capital-Raising","#Round-Process","#Previous-Capital-Raised");
 
             // status check: submit application
             statusSubmitApplication("#Tagline","#Business-Model","#Is-your-product-live-in-the-market","#Key-Metrics","#Founder-Team-Background","#Unfair-Advantage","#Pricing-Model","#customer-acquisition-sources","#Competition-Positioning","#Why-Rise-Capital","#Capital-Raising","#Round-Process","#Previous-Capital-Raised");
@@ -371,19 +406,19 @@ setupAutosaveField("#Capital-Legal-Name", null, "Company Legal Name","company-le
 statusValueProposition("#Tagline","#Business-Model","#Is-your-product-live-in-the-market");
 
 // status check: traction & metrics
-statusTractionMetrics("#Key-Metrics","#status-tractionmetrics-incomplete","#status-tractionmetrics-complete");
+statusTractionMetrics("#Key-Metrics");
 
 // status check: my team
-statusTeam("#Founder-Team-Background","#Unfair-Advantage","#status-team-incomplete","#status-team-complete");
+statusTeam("#Founder-Team-Background","#Unfair-Advantage");
 
 // status check: pricing & customers
-statusPricing("#Pricing-Model","#customer-acquisition-sources","#status-pricing-incomplete","#status-pricing-complete");
+statusPricing("#Pricing-Model","#customer-acquisition-sources");
 
 // status check: competition
-statusCompetition("#Competition-Positioning","#status-competition-incomplete","#status-competition-complete");
+statusCompetition("#Competition-Positioning");
 
 // status check: fundraising
-statusFundraising("#Why-Rise-Capital","#Capital-Raising","#Round-Process","#Previous-Capital-Raised","#status-fundraising-incomplete","#status-fundraising-complete");
+statusFundraising("#Why-Rise-Capital","#Capital-Raising","#Round-Process","#Previous-Capital-Raised");
 
 // status check: submit application
 statusSubmitApplication("#Tagline","#Business-Model","#Is-your-product-live-in-the-market","#Key-Metrics","#Founder-Team-Background","#Unfair-Advantage","#Pricing-Model","#customer-acquisition-sources","#Competition-Positioning","#Why-Rise-Capital","#Capital-Raising","#Round-Process","#Previous-Capital-Raised");
