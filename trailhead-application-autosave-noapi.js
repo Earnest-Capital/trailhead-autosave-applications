bdc483b7e16aeafc1bcb1a6394d3840050cfb5b1
Michael Rouveure <michaelrouveure@hotmail.com>
2022-07-13 17:44:57 -0600

    fix

--- trailhead-application-autosave-noapi.js
+++ trailhead-application-autosave-noapi.js
@@ -285,16 +285,16 @@ function statusBreakEven(inputBreakEven)
   console.log(fieldBreakEven.value);
   
   
-if (!fieldBreakEven.value === "Yes") 
+if (fieldBreakEven.value === "Yes" || "Select one...") 
 {
-  //show the div that contains the burn rate question
-  document.querySelector("#div-burn-rate").style.display = "flex";
+  //hide the div that contains the burn rate question
+  document.querySelector("#div-burn-rate").style.display = "none";
 
 }
 else
 {
-   //hide the div that contains the burn rate question
-  document.querySelector("#div-burn-rate").style.display = "none";
+   //show the div that contains the burn rate question
+  document.querySelector("#div-burn-rate").style.display = "flex";
 
 }
 
